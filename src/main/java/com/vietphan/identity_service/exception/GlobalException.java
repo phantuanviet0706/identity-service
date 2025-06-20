90b52ae5849529dcdc2409e4712db985160f3987
vietphan565 <phantuanviet0706@gmail.com>
2025-06-20 09:51:29 +0700

    Init sample project

---
+++ src/main/java/com/vietphan/identity_service/exception/GlobalException.java
@@ -0,0 +1,15 @@
+package com.vietphan.identity_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<String> handlingRuntimeException(RuntimeException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+}
---
+++ src/main/java/com/vietphan/identity_service/service/UserService.java
@@ -0,0 +1,55 @@
+package com.vietphan.identity_service.service;
+
+import com.vietphan.identity_service.dto.request.UserCreationRequest;
+import com.vietphan.identity_service.dto.request.UserUpdateRequest;
+import com.vietphan.identity_service.entity.User;
+import com.vietphan.identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, UserUpdateRequest request) {
+        User user = getUser(id);
+
+        user.setPassword(request.getPassword());
+        user.setLastName(request.getLastName());
+        user.setFirstName(request.getFirstName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
